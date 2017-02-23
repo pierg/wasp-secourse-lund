523c23bdf3300d2e9a7176e3db4ae0a3834188ee
Piergiuseppe Mallozzi <piergiuseppe.mallozzi@gmail.com>
2017-02-23 17:22:44 +0100

    Fixes #4

--- src/pigtranslator.py
+++ src/pigtranslator.py
@@ -18,3 +18,10 @@ def Translate(sentence):
 
 def t(str):
        return str[0]+str[1]
+
+
+if __name__ == "__main__":
+       english_sentence = input('Type what you would like translated into pig-latin and press ENTER: ')
+       print(english_sentence)
+       pig_sentence = Translate(english_sentence)
+       print(pig_sentence)
