f5e1fd72a159fd1f279b8cd3d4312e5e593b026b
David Issa Mattos <davidis@cse-3395760.local>
2017-02-23 14:05:49 +0100

    adding unittest to the app

--- src/tests.py
+++ src/tests.py
@@ -2,15 +2,17 @@ import unittest
 from pigtranslator import Translate
 
 class TestTranslate(unittest.TestCase):
-
+    #For rule 1 in Wikipedia
     def test_consonantSounds(self):
         s = 'pig'
         self.assertEqual(Translate(s), 'igpay')
 
+    #For rule 2 in Wikipedia
     def test_consonantClusters(self):
          s = 'cheers'
          self.assertEqual(Translate(s), 'eerschay')
-         
+    
+    #For rule 3 in Wikipedia
     def test_vowelSounds(self):
          s = 'eat'
          self.assertEqual(Translate(s), 'eatway')
